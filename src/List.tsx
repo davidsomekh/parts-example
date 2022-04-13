cced8c3624bf11fcf493676bbf96504669667365
davidsomekh <67224401+davidsomekh@users.noreply.github.com>
2022-04-13 14:03:26 +0300

    parts structure

--- src/List.tsx
+++ src/List.tsx
@@ -1,17 +1,43 @@
 import React, { useEffect, useState } from "react";
 
 import logo from './logo.svg';
+import Part from './Part';
 
 import './App.css';
 
+
+
 function List() {
 
+  const [parts,setParts] = useState([
+    {
+      name: "Wheel",
+      amount: 8,
+      file_name: "wheel.sldprt"
+    },
+    {
+      name: "Chair",
+      amount:2,
+      file_name: "chair new.sldprt"
+    },
+    {
+      name: "Engine",
+      amount: 1,
+      file_name: "engine_v1.sldprt"
+    },
+  ]);
 
+  const getParts= () => {
+
+    return '<div>g</div>'
+
+   }
 
   return (
     <div className="list">
-    
+      
       existing parts show here
+      {getParts()}
     </div>
   );
 }
