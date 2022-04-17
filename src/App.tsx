8580f7dd1f02581b781e1cebb03efb1a86124c58
davidsomekh <67224401+davidsomekh@users.noreply.github.com>
2022-04-17 12:32:08 +0300

    validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,72 @@
 import React, { useEffect, useState } from "react";
 
-import logo from './logo.svg';
+import logo from "./logo.svg";
 
-import './App.css';
-import Newpart from './New';
-import List from './List';
+import "./App.css";
+import Newpart from "./New";
+import List from "./List";
 import { stringify } from "querystring";
 
-
 function App() {
-
-  function makeid(length:number) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  function makeid(length: number) {
+    var result = "";
+    var characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * 
- charactersLength));
-   }
-   return result;
-}
-
-interface newpart {
-  name:string,
-  amount:number,
-  id:string,
-  file_name:string
-}
-
-  const [parts, setParts] = useState<newpart[]>([]); 
-
-  const getID = () =>
-  {
-    return 'dave';
+    for (var i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
   }
 
-  const addPart = (name:string,amount:number,fileName:string) => {
-    let part = {name:name,amount:amount,file_name:fileName,id:makeid(6)};
-    setParts([...parts,part])
-   // setParts{...parts,part};
-   }
+  interface newpart {
+    name: string;
+    amount: number;
+    id: string;
+    file_name: string;
+  }
 
+   const [parts, setParts] = useState<newpart[]>([]);
+  const [error, setError] = useState("");
 
-  const [newPart,setnewPart] = useState(false);
+  const showError = (error: string) => {
+    setError(error);
+  };
 
-  const showFileAdd= () => {
+  const addPart = (name: string, amount: number, fileName: string) => {
+    let part = {
+      name: name,
+      amount: amount,
+      file_name: fileName,
+      id: makeid(6),
+    };
+    setParts([...parts, part]);
+    // setParts{...parts,part};
+  };
 
+  const [newPart, setnewPart] = useState(false);
+
+  const showFileAdd = () => {
     setnewPart(true);
+  };
 
-   }
-
-   const hideFileAdd = () => {
+  const hideFileAdd = () => {
     setnewPart(false);
-   }
-
-   
-
+  };
 
   return (
     <div className="App">
-    <div className='head'>Parts</div>
-    <List parts={parts}/>
-    {parts.length == 0 && 'Click below to add new parts!'}
-    <div onClick={showFileAdd}className='btn'>+ Add New</div>
-    
-    {newPart && <Newpart close ={hideFileAdd} upload={addPart}/>}
+      {error.length > 0 && <div className="error">{error}</div>}
+      <div className="head">Parts</div>
+      <List parts={parts} />
+      {parts.length == 0 && "Click below to add new parts!"}
+      <div onClick={showFileAdd} className="btn">
+        + Add New
+      </div>
+
+      {newPart && (
+        <Newpart error={showError} close={hideFileAdd} upload={addPart} />
+      )}
     </div>
   );
 }
--- src/New.tsx
+++ src/New.tsx
@@ -1,39 +1,73 @@
 import React, { useEffect, useState } from "react";
 
-import logo from './logo.svg';
+import logo from "./logo.svg";
 
-import './App.css';
+import "./App.css";
 
-function Newpart(props:any) {
+function Newpart(props: any) {
+  const [name, setName] = useState("");
+  const [amount, setAmount] = useState(1);
 
-  const [name,setName] = useState('');
-  const [amount,setAmount] = useState(1);
+  const onUpload = () => {
+    if (!validate()) return;
 
-  const onUpload = () =>{
-      props.upload(name,amount,"key.pdf");
-      reset();
-  }
+    props.upload(name, amount, "key.pdf");
+    reset();
+  };
 
-  const reset = () =>{
+  const validate = () => {
+    if (name == "") {
+      props.error("Please select name");
+      return false;
+    }
+    if (amount < 1) {
+      props.error("Amount should be at least 1!");
+      return false;
+    }
+
+    props.error("");
+
+    return true;
+  };
+
+  const reset = () => {
     props.close();
-  }
+  };
 
+  const handleNameChange = (e: any) => {
+    setName(e.target.value);
+  };
 
-  const handleNameChange = (e:any) => {
-    setName( e.target.value);
-}
-
-const handlAmountChange = (e:any) => {
-  setAmount( e.target.value);
-}
+  const handlAmountChange = (e: any) => {
+    setAmount(e.target.value);
+  };
 
   return (
     <div className="new">
-    
-      <div> <input value={name}  onChange={(e)=>handleNameChange(e)} placeholder="Name" type="text"></input></div>
-      <div> <input value={amount}  onChange={(e)=>handlAmountChange(e)} placeholder="Amount" type="number"></input></div>
-      <div><input type="file" id="myfile" name="myfile"></input></div>
-      <div onClick={onUpload} className="btn upload">Upload</div>
+      <div>
+        {" "}
+        <input
+          value={name}
+          onChange={(e) => handleNameChange(e)}
+          placeholder="Name"
+          type="text"
+        ></input>
+      </div>
+      <div>
+        {" "}
+        <input
+          value={amount}
+          onChange={(e) => handlAmountChange(e)}
+          placeholder="Amount"
+          type="number"
+        ></input>
+      </div>
+      <div>
+        <input type="file" id="myfile" name="myfile"></input>
+      </div>
+      <div onClick={onUpload} className="btn upload">
+        Upload
+      </div>
     </div>
   );
 }
